2ed968efd9a9527756d186b77e3f8b7d9d6da485
Abdullah Al Noman <101085500+Al-Noman-12@users.noreply.github.com>
2023-11-04 18:20:36 +0600

    Update twrp_a22.mk

--- twrp_a22.mk
+++ twrp_a22.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -23,7 +23,3 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a22nsxx-user 12 SP1A.210812.016 A225FXXU6DWF3 release-keys"
-
-BUILD_FINGERPRINT := samsung/a22nsxx/a22:12/SP1A.210812.016/A225FXXU6DWF3:user/release-keys
