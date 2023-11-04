bfbba7f499c798bcee926fa9b5f30aa321a01e7c
Abdullah Al Noman <101085500+Al-Noman-12@users.noreply.github.com>
2023-11-04 18:23:45 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,24 @@
 #
 
 LOCAL_PATH := device/samsung/a22
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+ fastbootd \
+ android.hardware.fastboot@1.0-impl-mock \
+ android.hardware.fastboot@1.0-impl-mock.recovery
+
+PRODUCT_PRODUCT_PROPERTIES += \
+ ro.fastbootd.available=true
+
+# Boot Control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-mtkimpl.recovery \
+    android.hardware.boot@1.1-mtkimpl \
+    bootctl
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl.mt6768
